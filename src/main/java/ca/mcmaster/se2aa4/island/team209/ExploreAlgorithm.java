d7d11c820dec23f913d8005a8dac01e6c8827fc8
xSankalpaD <chhets1@mcmaster.ca>
2024-02-25 16:03:23 -0500

    Island scanning setup

--- src/main/java/ca/mcmaster/se2aa4/island/team209/ExploreAlgorithm.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team209/ExploreAlgorithm.java
@@ -2,5 +2,6 @@ package ca.mcmaster.se2aa4.island.team209;
 
 public interface ExploreAlgorithm {
     public void topLeftIslandFinder();
+
     public void IslandTransversal();
 }
--- src/main/java/ca/mcmaster/se2aa4/island/team209/Map.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team209/Map.java
@@ -1,5 +1,41 @@
 package ca.mcmaster.se2aa4.island.team209;
 
+import eu.ace_design.island.game.PointOfInterest;
+import java.util.*;
+
 public class Map {
+    private int[][] terrain; // Representation of the terrain
+    private List<Point> creeks; // List of creeks on the island
+    private Point emergencySite; // Location of the emergency site
+
+    public Map(int[][] terrain, List<Point> creeks, Point emergencySite) {
+        this.terrain = terrain;
+        this.creeks = creeks;
+        this.emergencySite = emergencySite;
+    }
+
+    public int[][] getTerrain() {
+        return terrain;
+    }
+
+    public void setTerrain(int[][] terrain) {
+        this.terrain = terrain;
+    }
+
+    public List<Point> getCreeks() {
+        return creeks;
+    }
+
+    public void setCreeks(List<Point> creeks) {
+        this.creeks = creeks;
+    }
+
+    public Point getEmergencySite() {
+        return emergencySite;
+    }
+
+    public void setEmergencySite(Point emergencySite) {
+        this.emergencySite = emergencySite;
+    }
 
 }
