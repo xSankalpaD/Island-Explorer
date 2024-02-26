ebca149213dcef432324364c85c1fc4ddc974166
Sankalpa <90166191+xSankalpaD@users.noreply.github.com>
2024-02-25 22:28:36 -0500

    Added value in Point class #12

--- src/main/java/ca/mcmaster/se2aa4/island/team209/Point.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team209/Point.java
@@ -3,10 +3,12 @@ package ca.mcmaster.se2aa4.island.team209;
 public class Point {
     public int x;
     public int y;
+    public int value;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+        this.value = value;
     }
 
 }
