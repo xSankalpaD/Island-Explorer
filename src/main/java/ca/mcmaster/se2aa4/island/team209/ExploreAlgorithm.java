00931741472dbb00e52a3dfc0363d729f8aef2fb
Kevin <zhuk41@mcmaster.ca>
2024-02-29 00:25:30 -0500

    Made drone find and successfully arrive at island using island finding algorithm #13

--- src/main/java/ca/mcmaster/se2aa4/island/team209/ExploreAlgorithm.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team209/ExploreAlgorithm.java
@@ -1,7 +1,7 @@
 package ca.mcmaster.se2aa4.island.team209;
 
 public interface ExploreAlgorithm {
-    public String decision();
-    public void takeInfo(String info);
-    public String finalReport();
+     String decision();
+     void takeInfo(String info);
+     String finalReport();
 }
