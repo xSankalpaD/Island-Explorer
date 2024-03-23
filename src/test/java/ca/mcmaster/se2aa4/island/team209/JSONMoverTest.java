02ca179c1db00ddfcab8bc2d6c6ae75fe465c493
Kevin <zhuk41@mcmaster.ca>
2024-03-22 21:36:54 -0400

    Added additional tests #11

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team209/JSONMoverTest.java
@@ -0,0 +1,48 @@
+package ca.mcmaster.se2aa4.island.team209;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JSONMoverTest {
+    Movement mover;
+    ExploringDrone drone;
+    @BeforeEach
+    public void init(){
+        drone = new ExploringDrone(0,0, 10000, Direction.E);
+        mover = new JSONMover(drone);
+    }
+    @Test
+    public void emptyTest(){
+        assertTrue(mover.needsInstruction());
+        mover.goForward();
+        assertFalse(mover.needsInstruction());
+        mover.getNextInstruction();
+        assertTrue(mover.needsInstruction());
+    }
+    @Test
+    public void orderTest(){
+        mover.goForward();
+        mover.goRight();
+        mover.stop();
+        String s= mover.getNextInstruction();
+        assertTrue(s.contains("fly"));
+        s = mover.getNextInstruction();
+        assertTrue(s.contains("heading")&&s.contains("S"));
+        s = mover.getNextInstruction();
+        assertTrue(s.contains("stop"));
+    }
+    @Test
+    public void radarScanTest(){
+        mover.useRadar(drone.getDirection());
+        mover.scan();
+        String s = mover.getNextInstruction();
+        assertTrue(s.contains("echo") &&
+                s.contains("E"));
+        s = mover.getNextInstruction();
+        assertTrue(s.contains("scan"));
+    }
+}
